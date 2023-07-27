fd2469f4404ec9088676f2d8dbbac13f7900578b
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-27 11:56:35 -0600

    Added freezable module
    Necessary to ensure objects are frozen after being initialized. This module is specialized for being *prepended* in order to ensure proper freezing of an object.
    
    Reek has been updated to ignore module initialization which, under normal circumstances, would be valid but this module is designed for prepending and needs this behavior to work.

--- lib/wholable.rb
+++ lib/wholable.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "wholable/comparable"
+require "wholable/freezable"
 
 # Main namespace.
 module Wholable
---
+++ lib/wholable/freezable.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Wholable
+  # Ensures an object is frozen after being initialized.
+  module Freezable
+    def initialize(...)
+      super(...)
+      freeze
+    end
+  end
+end
